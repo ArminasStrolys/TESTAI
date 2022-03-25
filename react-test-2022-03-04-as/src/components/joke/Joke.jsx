4c9636fa4ed7e4dab1af9bbf0ed62db145acd80a
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-25 10:35:49 +0200

    Fixes

--- react-test-2022-03-04-as/src/components/joke/Joke.jsx
+++ react-test-2022-03-04-as/src/components/joke/Joke.jsx
@@ -9,7 +9,11 @@ fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,pol
 // .then(data => console.log(data.jokes[1].joke))
 .then(data => data.jokes.map(e=>{
   console.log(e)
-  return setup.innerHTML += `<span>${e.joke}</span>`
+  if (e.type === 'single'){
+    return setup.innerHTML += `<span><b>Joke:</b> ${e.joke}</span>`
+  } else {
+    return setup.innerHTML += `<span><b>Setup:</b> ${e.setup}<hr/><b>Punchline:</b> ${e.delivery}</span>`
+  }
 }))
 
 export default function Joke() {
