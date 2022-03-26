0b853aa2ac625fb8f08b24542cb4b321fd2019eb
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-26 20:28:34 +0200

    fixes

--- react-test-2022-03-04-as/src/components/joke/Joke.jsx
+++ react-test-2022-03-04-as/src/components/joke/Joke.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import './joke.css'
 
-const setup = document.querySelector('div')
+// const setup = document.querySelector('div')
+
+let jokesHere = []
 
 fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=10')
 .then(res => res.json())
-.then(data => data.jokes.map(e=>{
-  console.log(e)
-  if (e.type === 'single'){
-    return setup.innerHTML += `<span><b>Joke:</b> ${e.joke}</span>`
-  } else {
-    return setup.innerHTML += `<span><b>Setup:</b> ${e.setup}<hr/><b>Punchline:</b> ${e.delivery}</span>`
-  }
+.then(data => jokesHere = data.jokes.map(e=>{
+  return e.type
 }))
+.then(() => console.log(jokesHere))
+// .then(data => data.jokes.map(e=>{
+//   console.log(e)
+//   if (e.type === 'single'){
+//     return setup.innerHTML += `<span><b>Joke:</b> ${e.joke}</span>`
+//   } else {
+//     return setup.innerHTML += `<span><b>Setup:</b> ${e.setup}<hr/><b>Punchline:</b> ${e.delivery}</span>`
+//   }
+// }))
 
 export default function Joke() {
-  return (
-    <>
-    </>
-  );
+  return <>
+  <div>
+  <p>HI</p>
+  </div>
+  </>
 }
